0e8eb298d247f0cdd1c5f56bea6d24dc422a978d
hyfly233 <hyfly233@outlook.com>
2017-11-28 12:00:00 +0800

    commit

---
+++ src/main/java/com/hyfly/http/sdk/core/BaseOperation.java
@@ -0,0 +1,25 @@
+package com.hyfly.http.sdk.core;
+
+import com.hyfly.http.sdk.remote.HttpSdkRestTemplate;
+
+public abstract class BaseOperation {
+
+    protected final String url;
+
+    protected final String token;
+
+    protected final HttpSdkRestTemplate restTemplate;
+
+    public BaseOperation(String url, String token, HttpSdkRestTemplate restTemplate) {
+        this.url = url;
+        this.token = token;
+        this.restTemplate = restTemplate;
+    }
+
+//    protected Header getHeader() {
+//        Header header = Header.newInstance();
+//        header.addParam("token", this.token);
+//        return header;
+//    }
+
+}
--- src/main/java/com/hyfly/http/sdk/remote/HttpSdkRestTemplate.java
+++ src/main/java/com/hyfly/http/sdk/remote/HttpSdkRestTemplate.java
@@ -1,4 +1,10 @@
 package com.hyfly.http.sdk.remote;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class HttpSdkRestTemplate {
+
+    Logger logger = LoggerFactory.getLogger(HttpSdkRestTemplate.class);
+
 }
