0027d7231d509e76167f5126a1d55d01631c565e
hyfly233 <hyfly233@outlook.com>
2019-04-19 12:00:00 +0800

    commit

---
+++ src/main/java/com/hyfly/http/sdk/remote/TypeReferenceHttpResult.java
@@ -0,0 +1,21 @@
+package com.hyfly.http.sdk.remote;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.lang.reflect.Type;
+
+public class TypeReferenceHttpResult<T> extends TypeReference<BaseHttpRestResult<T>> {
+
+    protected final Type type;
+
+    public TypeReferenceHttpResult(Class<?>... clazz) {
+        ObjectMapper mapper = new ObjectMapper();
+        type = mapper.getTypeFactory().constructParametricType(BaseHttpRestResult.class, clazz);
+    }
+
+    @Override
+    public Type getType() {
+        return type;
+    }
+}
--- src/main/java/com/hyfly/http/sdk/remote/handler/ResponseHandler.java
+++ src/main/java/com/hyfly/http/sdk/remote/handler/ResponseHandler.java
@@ -1,5 +1,12 @@
 package com.hyfly.http.sdk.remote.handler;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hyfly.http.sdk.remote.BaseHttpRestResult;
+import com.hyfly.http.sdk.remote.response.HttpClientResponse;
+import org.apache.http.HttpStatus;
+
+import java.io.InputStream;
+
 public class ResponseHandler<T> {
 
     private Class<T> responseType;
@@ -8,4 +15,25 @@ public class ResponseHandler<T> {
         this.responseType = responseType;
     }
 
+    public final BaseHttpRestResult<T> handle(HttpClientResponse response) throws Exception {
+
+        if (HttpStatus.SC_BAD_REQUEST < response.getStatusCode()) {
+            return handleError(response);
+        }
+        return convertResult(response, this.responseType);
+    }
+
+    public final BaseHttpRestResult<T> handleError(HttpClientResponse response) throws Exception {
+
+        return null;
+    }
+
+    public BaseHttpRestResult<T> convertResult(HttpClientResponse response, Class<T> responseType)
+            throws Exception {
+        InputStream is = response.getBody();
+
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.readValue(is, new TypeReferenceHttpResult<>(responseType));
+
+    }
 }
