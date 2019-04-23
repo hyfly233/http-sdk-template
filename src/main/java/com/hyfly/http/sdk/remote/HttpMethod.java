ae5b033f7360ed064f41d4b4c35bd67cea5129fc
hyfly233 <hyfly233@outlook.com>
2019-04-23 12:00:00 +0800

    commit

--- src/main/java/com/hyfly/http/sdk/remote/HttpMethod.java
+++ src/main/java/com/hyfly/http/sdk/remote/HttpMethod.java
@@ -1,20 +1,19 @@
 package com.hyfly.http.sdk.remote;
 
 public class HttpMethod {
+    public static final String GET = "GET";
 
-  public static final String GET = "GET";
+    public static final String HEAD = "HEAD";
 
-  public static final String HEAD = "HEAD";
+    public static final String POST = "POST";
 
-  public static final String POST = "POST";
+    public static final String PUT = "PUT";
 
-  public static final String PUT = "PUT";
+    public static final String PATCH = "PATCH";
 
-  public static final String PATCH = "PATCH";
+    public static final String DELETE = "DELETE";
 
-  public static final String DELETE = "DELETE";
+    public static final String OPTIONS = "OPTIONS";
 
-  public static final String OPTIONS = "OPTIONS";
-
-  public static final String TRACE = "TRACE";
+    public static final String TRACE = "TRACE";
 }
